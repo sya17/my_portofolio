f4b8708f071ccf398201c21d11c5a9cc22a031e4
Sarip Hidayatullah <102409123+sya17@users.noreply.github.com>
2023-07-31 15:53:03 +0700

    Change Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ export default async  function Home() {
       <main className="flex-1 w-full py-4 px-6 overflow-y-auto flex justify-center items-center ">
         <div className=" w-full font-bold text-gray-300 flex justify-center items-center">
           <div className="flex flex-col w-full justify-center items-center space-y-6 font-mono">
-            <span className="text-2xl">I&apos;M Sarip</span>
+            {/* <span className="text-2xl">I&apos;M Sarip</span> */}
+            <span className="text-2xl">Sarip Hidayatullah</span>
             <span className="typing-demo text-xl">Programmer Java Developer</span>
           </div>
         </div>
